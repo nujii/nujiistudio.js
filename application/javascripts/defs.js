ddd9a73dbd11d82db413683326692a1d726dbf2e
Cameron Bytheway <cameron@nujii.com>
2012-05-21 14:25:01 -0600

    first spine commit

--- application/javascripts/defs.js
+++
@@ -1,7 +0,0 @@
-
-goog.provide('nujii.studio.defs');
-
-nujii.studio.defs={};
-
-nujii.studio.defs.BASE_URL = '//cameron-bytheways-workstation:8000';
-
--- application/javascripts/lib/share.js
+++
@@ -1,5 +0,0 @@
-goog.provide('share');
-
-goog.require('share.bcsocket');
-goog.require('share.lib');
-goog.require('share.json');
